e30cfc335f95e3a6d92533c8b59c084bc96f1284
YujiAsai(浅井有志) <yuji.asai.a845@dirbato.co.jp>
2024-04-24 14:30:20 +0900

    4/24

--- first_webapp/src/filter/EncodingFilter.java
+++ first_webapp/src/filter/EncodingFilter.java
@@ -24,67 +24,29 @@ public class EncodingFilter extends HttpFilter implements Filter {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+        /**
+         * @see Filter#destroy()
+         */
+        public void destroy() {
+                // TODO Auto-generated method stub
+        }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-	    request.setCharacterEncoding("UTF-8");
-	    response.setCharacterEncoding("UTF-8");
-	    package filter;
+        /**
+         * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+         */
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                // TODO Auto-generated method stub
+                // place your code here
 
-	    import java.io.IOException;
-	    import java.time.LocalDateTime;
+                // pass the request along the filter chain
+                chain.doFilter(request, response);
+        }
 
-	    import javax.servlet.Filter;
-	    import javax.servlet.FilterChain;
-	    import javax.servlet.FilterConfig;
-	    import javax.servlet.ServletException;
-	    import javax.servlet.ServletRequest;
-	    import javax.servlet.ServletResponse;
-	    import javax.servlet.annotation.WebFilter;
-	    import javax.servlet.http.HttpFilter;
-	    import javax.servlet.http.HttpServletRequest;
-
-	    @WebFilter("/*")
-	    public class LogFilter extends HttpFilter implements Filter {
-
-	        public LogFilter() {
-	            super();
-	        }
-
-	        public void destroy() {
-	        }
-
-	        /**
-	         * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	         */
-	        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-	                throws IOException, ServletException {
-	            chain.doFilter(request, response);
-
-	            // アクセスされた場所と日時を記録する
-	            System.out.println(((HttpServletRequest) request).getRequestURI() + ":" + LocalDateTime.now());
-	        }
-
-	        public void init(FilterConfig fConfig) throws ServletException {
-	        }
-
-	    }
-	    chain.doFilter(request, response);
-	}
-
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
+        /**
+         * @see Filter#init(FilterConfig)
+         */
+        public void init(FilterConfig fConfig) throws ServletException {
+                // TODO Auto-generated method stub
+        }
 
 }
